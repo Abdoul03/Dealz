6c03fbb33e593a59d048358707791764103e760c
Abdoul03 <samabdoul03@gmail.com>
2026-05-29 22:30:17 +0000

    first commit

---
+++ src/main/java/com/doul/dealz/DealzApplication.java
@@ -0,0 +1,13 @@
+package com.doul.dealz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doul/dealz/DealzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doul.dealz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
